2ed9e550e01baed23b2fb7777204e3b86d44fcdd
Dmx DarkFire <131019302+Lebgdj12@users.noreply.github.com>
2026-05-02 23:14:50 +0200

    Update Fuck.js

--- Fuck.js
+++ Fuck.js
@@ -1,96 +1,84 @@
 Java.perform(function() {
 
-    // 1. Cacher Frida dans /proc/self/maps
+    // 1. Dialog standard
     try {
-        var fopen = Module.findExportByName(null, "fopen");
-        Interceptor.attach(fopen, {
-            onEnter: function(args) {
-                var path = args[0].readCString();
-                if (path && (path.indexOf("/proc/") !== -1 && path.indexOf("/maps") !== -1)) {
-                    this.isMaps = true;
+        var Dialog = Java.use("android.app.Dialog");
+        Dialog.show.implementation = function() {
+            var t = ""; try { t = this.getTitle(); } catch(e) {}
+            var c = ""; try { c = this.getClass().getName(); } catch(e) {}
+            console.log("[NP] Dialog titre=" + t + " classe=" + c);
+            this.show();
+        };
+    } catch(e) { console.log("[NP] err Dialog: " + e); }
+
+    // 2. AlertDialog.Builder
+    try {
+        var Builder = Java.use("android.app.AlertDialog$Builder");
+        Builder.show.overload().implementation = function() {
+            console.log("[NP] AlertDialog.Builder");
+            this.show();
+        };
+    } catch(e) {}
+
+    // 3. PopupWindow
+    try {
+        var Popup = Java.use("android.widget.PopupWindow");
+        Popup.showAtLocation.implementation = function() {
+            console.log("[NP] PopupWindow.showAtLocation");
+            this.showAtLocation.apply(this, arguments);
+        };
+        Popup.showAsDropDown.implementation = function() {
+            console.log("[NP] PopupWindow.showAsDropDown");
+            this.showAsDropDown.apply(this, arguments);
+        };
+    } catch(e) {}
+
+    // 4. Activity en style dialog
+    try {
+        var Activity = Java.use("android.app.Activity");
+        Activity.onResume.implementation = function() {
+            var name = this.getClass().getName();
+            console.log("[NP] Activity onResume: " + name);
+            this.onResume();
+        };
+    } catch(e) {}
+
+    // 5. WindowManager.addView (overlay)
+    try {
+        var WM = Java.use("android.view.WindowManagerImpl");
+        WM.addView.overload("android.view.View", "android.view.ViewGroup$LayoutParams").implementation = function(view, params) {
+            var type = ""; try { type = "" + params.type; } catch(e) {}
+            console.log("[NP] addView type=" + type);
+            this.addView(view, params);
+        };
+    } catch(e) {}
+
+    // 6. Toast
+    try {
+        var Toast = Java.use("android.widget.Toast");
+        Toast.show.implementation = function() {
+            var text = ""; try { text = this.getView().findViewById(16908308).getText(); } catch(e) {}
+            console.log("[NP] Toast: " + text);
+            this.show();
+        };
+    } catch(e) {}
+
+    // 7. Enumerer les classes NP Manager
+    try {
+        Java.enumerateLoadedClasses({
+            onMatch: function(name) {
+                if (name.startsWith("player.normal") || name.startsWith("com.wn")) {
+                    var l = name.toLowerCase();
+                    if (l.indexOf("update") !== -1 || l.indexOf("upgrade") !== -1 || 
+                        l.indexOf("force") !== -1 || l.indexOf("version") !== -1 ||
+                        l.indexOf("popup") !== -1 || l.indexOf("notice") !== -1 ||
+                        l.indexOf("dialog") !== -1 || l.indexOf("alert") !== -1) {
+                        console.log("[NP] CLASSE: " + name);
+                    }
                 }
             },
-            onLeave: function(retval) {
-                if (this.isMaps) {
-                    var fgets = Module.findExportByName(null, "fgets");
-                    if (fgets) {
-                        Interceptor.attach(fgets, {
-                            onEnter: function(args) {},
-                            onLeave: function(retval) {
-                                try {
-                                    var line = args[1].readCString();
-                                    if (line && (line.indexOf("frida") !== -1 || line.indexOf("gadget") !== -1 || line.indexOf("linjector") !== -1)) {
-                                        args[1].writeCString("");
-                                    }
-                                } catch(e) {}
-                            }
-                        });
-                    }
-                }
-            }
+            onComplete: function() { console.log("[NP] Scan fini"); }
         });
     } catch(e) {}
-
-    // 2. Bloquer connexion au port Frida
-    try {
-        var connect = Module.findExportByName(null, "connect");
-        Interceptor.attach(connect, {
-            onEnter: function(args) {
-                try {
-                    var addr = args[1].readByteArray(8);
-                    var port = (new Uint8Array(addr)[2] << 8) | new Uint8Array(addr)[3];
-                    if (port === 27042 || port === 27043) {
-                        args[2] = ptr(0);
-                    }
-                } catch(e) {}
-            }
-        });
-    } catch(e) {}
-
-    // 3. Cacher les threads Frida
-    try {
-        var opendir = Module.findExportByName(null, "opendir");
-        Interceptor.attach(opendir, {
-            onEnter: function(args) {
-                try {
-                    var path = args[0].readCString();
-                    if (path && path.indexOf("/proc/") !== -1 && path.indexOf("/task") !== -1) {
-                        this.isTask = true;
-                    }
-                } catch(e) {}
-            }
-        });
-    } catch(e) {}
-
-    // 4. Hook Dialog.show APRES un délai
-    setTimeout(function() {
-        Java.perform(function() {
-            try {
-                var Dialog = Java.use("android.app.Dialog");
-                Dialog.show.implementation = function() {
-                    var title = "";
-                    try { title = this.getTitle(); } catch(e) {}
-                    var cls = "";
-                    try { cls = this.getClass().getName(); } catch(e) {}
-                    console.log("[NP] Dialog titre=" + title + " classe=" + cls);
-                    this.show();
-                };
-            } catch(e) {}
-
-            try {
-                Java.enumerateLoadedClasses({
-                    onMatch: function(name) {
-                        if (name.startsWith("player.normal") || name.startsWith("com.wn")) {
-                            var lower = name.toLowerCase();
-                            if (lower.indexOf("update") !== -1 || lower.indexOf("upgrade") !== -1 || lower.indexOf("force") !== -1 || lower.indexOf("version") !== -1) {
-                                console.log("[NP] CLASSE: " + name);
-                            }
-                        }
-                    },
-                    onComplete: function() {}
-                });
-            } catch(e) {}
-        });
-    }, 3000);
 
 });
