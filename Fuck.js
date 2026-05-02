c27b06d624600aabaf45b4b06163490944f6fda1
Dmx DarkFire <131019302+Lebgdj12@users.noreply.github.com>
2026-05-02 23:05:27 +0200

    Update Fuck.js

--- Fuck.js
+++ Fuck.js
@@ -1,51 +1,96 @@
 Java.perform(function() {
 
-    // 1. Hook Dialog.show() pour logger et bloquer
+    // 1. Cacher Frida dans /proc/self/maps
     try {
-        var Dialog = Java.use("android.app.Dialog");
-        Dialog.show.implementation = function() {
-            var title = "";
-            try { title = this.getTitle(); } catch(e) {}
-            var cls = "";
-            try { cls = this.getClass().getName(); } catch(e) {}
-            console.log("[NP] Dialog titre=" + title + " classe=" + cls);
-            this.show();
-        };
-    } catch(e) { console.log("[NP] Dialog err: " + e); }
-
-    // 2. Hook AlertDialog.Builder
-    try {
-        var Builder = Java.use("android.app.AlertDialog$Builder");
-        Builder.show.overload().implementation = function() {
-            console.log("[NP] AlertDialog.Builder.show()");
-            this.show();
-        };
-    } catch(e) {}
-
-    // 3. Chercher les classes update/force dans l'app
-    try {
-        Java.enumerateLoadedClasses({
-            onMatch: function(name) {
-                if (name.toLowerCase().indexOf("update") !== -1 ||
-                    name.toLowerCase().indexOf("upgrade") !== -1 ||
-                    name.toLowerCase().indexOf("force") !== -1 ||
-                    name.toLowerCase().indexOf("versioncheck") !== -1) {
-                    if (name.startsWith("com.wn") || name.startsWith("player")) {
-                        console.log("[NP] CLASSE TROUVEE: " + name);
-                    }
+        var fopen = Module.findExportByName(null, "fopen");
+        Interceptor.attach(fopen, {
+            onEnter: function(args) {
+                var path = args[0].readCString();
+                if (path && (path.indexOf("/proc/") !== -1 && path.indexOf("/maps") !== -1)) {
+                    this.isMaps = true;
                 }
             },
-            onComplete: function() {}
+            onLeave: function(retval) {
+                if (this.isMaps) {
+                    var fgets = Module.findExportByName(null, "fgets");
+                    if (fgets) {
+                        Interceptor.attach(fgets, {
+                            onEnter: function(args) {},
+                            onLeave: function(retval) {
+                                try {
+                                    var line = args[1].readCString();
+                                    if (line && (line.indexOf("frida") !== -1 || line.indexOf("gadget") !== -1 || line.indexOf("linjector") !== -1)) {
+                                        args[1].writeCString("");
+                                    }
+                                } catch(e) {}
+                            }
+                        });
+                    }
+                }
+            }
         });
     } catch(e) {}
 
-    // 4. Hook setContentView pour detecter le dialog layout
+    // 2. Bloquer connexion au port Frida
     try {
-        var Activity = Java.use("android.app.Activity");
-        Activity.setContentView.overload("int").implementation = function(id) {
-            console.log("[NP] setContentView id=" + id);
-            this.setContentView(id);
-        };
+        var connect = Module.findExportByName(null, "connect");
+        Interceptor.attach(connect, {
+            onEnter: function(args) {
+                try {
+                    var addr = args[1].readByteArray(8);
+                    var port = (new Uint8Array(addr)[2] << 8) | new Uint8Array(addr)[3];
+                    if (port === 27042 || port === 27043) {
+                        args[2] = ptr(0);
+                    }
+                } catch(e) {}
+            }
+        });
     } catch(e) {}
+
+    // 3. Cacher les threads Frida
+    try {
+        var opendir = Module.findExportByName(null, "opendir");
+        Interceptor.attach(opendir, {
+            onEnter: function(args) {
+                try {
+                    var path = args[0].readCString();
+                    if (path && path.indexOf("/proc/") !== -1 && path.indexOf("/task") !== -1) {
+                        this.isTask = true;
+                    }
+                } catch(e) {}
+            }
+        });
+    } catch(e) {}
+
+    // 4. Hook Dialog.show APRES un délai
+    setTimeout(function() {
+        Java.perform(function() {
+            try {
+                var Dialog = Java.use("android.app.Dialog");
+                Dialog.show.implementation = function() {
+                    var title = "";
+                    try { title = this.getTitle(); } catch(e) {}
+                    var cls = "";
+                    try { cls = this.getClass().getName(); } catch(e) {}
+                    console.log("[NP] Dialog titre=" + title + " classe=" + cls);
+                    this.show();
+                };
+            } catch(e) {}
+
+            try {
+                Java.enumerateLoadedClasses({
+                    onMatch: function(name) {
+                        if (name.startsWith("player.normal") || name.startsWith("com.wn")) {
+                            var lower = name.toLowerCase();
+                            if (lower.indexOf("update") !== -1 || lower.indexOf("upgrade") !== -1 || lower.indexOf("force") !== -1 || lower.indexOf("version") !== -1) {
+                                console.log("[NP] CLASSE: " + name);
+                            }
+                        }
+                    },
+                    onComplete: function() {}
+                });
+            } catch(e) {}
+        });
+    }, 3000);
 
 });
