b23a3e2157a6e25ac21cde559727433c22ab4493
Dmx DarkFire <131019302+Lebgdj12@users.noreply.github.com>
2026-05-03 00:11:18 +0200

    Update Fuck.js

--- Fuck.js
+++ Fuck.js
@@ -1,31 +1,68 @@
 Java.perform(function() {
-    var Log = Java.use("android.util.Log");
-    Log.e("NP-HOOK", "Script actif");
+    var L = Java.use("android.util.Log");
+    L.e("NP", "Script actif");
 
+    // Cacher frida dans les strings
     try {
-        var Dialog = Java.use("android.app.Dialog");
-        Dialog.show.implementation = function() {
+        var String = Java.use("java.lang.String");
+        String.contains.overload("java.lang.CharSequence").implementation = function(s) {
+            var check = s.toString();
+            if (check.indexOf("frida") >= 0 || check.indexOf("gadget") >= 0 || 
+                check.indexOf("xposed") >= 0 || check.indexOf("jshook") >= 0) {
+                return false;
+            }
+            return this.contains(s);
+        };
+    } catch(e) {}
+
+    // Cacher fichiers frida/jshook
+    try {
+        var File = Java.use("java.io.File");
+        File.exists.implementation = function() {
+            var p = this.getAbsolutePath();
+            if (p.indexOf("frida") !== -1 || p.indexOf("jshook") !== -1 || 
+                p.indexOf("jsonet") !== -1 || p.indexOf("re.zyg") !== -1) {
+                return false;
+            }
+            return this.exists();
+        };
+    } catch(e) {}
+
+    // Hook Dialog
+    try {
+        var D = Java.use("android.app.Dialog");
+        D.show.implementation = function() {
             var t = ""; try { t = this.getTitle(); } catch(e) {}
             var c = ""; try { c = this.getClass().getName(); } catch(e) {}
-            Log.e("NP-HOOK", "Dialog titre=" + t + " classe=" + c);
+            L.e("NP", "Dialog titre=" + t + " classe=" + c);
             this.show();
         };
-    } catch(e) { Log.e("NP-HOOK", "err Dialog: " + e); }
+    } catch(e) {}
 
+    // Hook Activity
+    try {
+        var A = Java.use("android.app.Activity");
+        A.onResume.implementation = function() {
+            L.e("NP", "Activity=" + this.getClass().getName());
+            this.onResume();
+        };
+    } catch(e) {}
+
+    // Scanner classes
     try {
         Java.enumerateLoadedClasses({
-            onMatch: function(name) {
-                if (name.startsWith("player.normal") || name.startsWith("com.wn")) {
-                    var l = name.toLowerCase();
-                    if (l.indexOf("update") !== -1 || l.indexOf("upgrade") !== -1 || 
-                        l.indexOf("force") !== -1 || l.indexOf("version") !== -1 ||
+            onMatch: function(n) {
+                if (n.startsWith("player.normal") || n.startsWith("com.wn")) {
+                    var l = n.toLowerCase();
+                    if (l.indexOf("update") !== -1 || l.indexOf("force") !== -1 || 
+                        l.indexOf("version") !== -1 || l.indexOf("dialog") !== -1 ||
                         l.indexOf("popup") !== -1 || l.indexOf("notice") !== -1 ||
-                        l.indexOf("dialog") !== -1) {
-                        Log.e("NP-HOOK", "CLASSE: " + name);
+                        l.indexOf("advert") !== -1) {
+                        L.e("NP", "CLASSE=" + n);
                     }
                 }
             },
-            onComplete: function() { Log.e("NP-HOOK", "Scan fini"); }
+            onComplete: function() { L.e("NP", "ScanFini"); }
         });
-    } catch(e) { Log.e("NP-HOOK", "err scan: " + e); }
+    } catch(e) {}
 });
