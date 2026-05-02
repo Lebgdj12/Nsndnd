dce95f314b7b4c291783a71324f067ca960d5620
Dmx DarkFire <131019302+Lebgdj12@users.noreply.github.com>
2026-05-02 23:24:27 +0200

    Update Fuck.js

--- Fuck.js
+++ Fuck.js
@@ -1,69 +1,16 @@
 Java.perform(function() {
+    send("[NP] Script actif");
 
-    // 1. Dialog standard
     try {
         var Dialog = Java.use("android.app.Dialog");
         Dialog.show.implementation = function() {
             var t = ""; try { t = this.getTitle(); } catch(e) {}
             var c = ""; try { c = this.getClass().getName(); } catch(e) {}
-            console.log("[NP] Dialog titre=" + t + " classe=" + c);
+            send("[NP] Dialog titre=" + t + " classe=" + c);
             this.show();
         };
-    } catch(e) { console.log("[NP] err Dialog: " + e); }
+    } catch(e) { send("[NP] err Dialog: " + e); }
 
-    // 2. AlertDialog.Builder
-    try {
-        var Builder = Java.use("android.app.AlertDialog$Builder");
-        Builder.show.overload().implementation = function() {
-            console.log("[NP] AlertDialog.Builder");
-            this.show();
-        };
-    } catch(e) {}
-
-    // 3. PopupWindow
-    try {
-        var Popup = Java.use("android.widget.PopupWindow");
-        Popup.showAtLocation.implementation = function() {
-            console.log("[NP] PopupWindow.showAtLocation");
-            this.showAtLocation.apply(this, arguments);
-        };
-        Popup.showAsDropDown.implementation = function() {
-            console.log("[NP] PopupWindow.showAsDropDown");
-            this.showAsDropDown.apply(this, arguments);
-        };
-    } catch(e) {}
-
-    // 4. Activity en style dialog
-    try {
-        var Activity = Java.use("android.app.Activity");
-        Activity.onResume.implementation = function() {
-            var name = this.getClass().getName();
-            console.log("[NP] Activity onResume: " + name);
-            this.onResume();
-        };
-    } catch(e) {}
-
-    // 5. WindowManager.addView (overlay)
-    try {
-        var WM = Java.use("android.view.WindowManagerImpl");
-        WM.addView.overload("android.view.View", "android.view.ViewGroup$LayoutParams").implementation = function(view, params) {
-            var type = ""; try { type = "" + params.type; } catch(e) {}
-            console.log("[NP] addView type=" + type);
-            this.addView(view, params);
-        };
-    } catch(e) {}
-
-    // 6. Toast
-    try {
-        var Toast = Java.use("android.widget.Toast");
-        Toast.show.implementation = function() {
-            var text = ""; try { text = this.getView().findViewById(16908308).getText(); } catch(e) {}
-            console.log("[NP] Toast: " + text);
-            this.show();
-        };
-    } catch(e) {}
-
-    // 7. Enumerer les classes NP Manager
     try {
         Java.enumerateLoadedClasses({
             onMatch: function(name) {
@@ -72,13 +19,12 @@ Java.perform(function() {
                     if (l.indexOf("update") !== -1 || l.indexOf("upgrade") !== -1 || 
                         l.indexOf("force") !== -1 || l.indexOf("version") !== -1 ||
                         l.indexOf("popup") !== -1 || l.indexOf("notice") !== -1 ||
-                        l.indexOf("dialog") !== -1 || l.indexOf("alert") !== -1) {
-                        console.log("[NP] CLASSE: " + name);
+                        l.indexOf("dialog") !== -1) {
+                        send("[NP] CLASSE: " + name);
                     }
                 }
             },
-            onComplete: function() { console.log("[NP] Scan fini"); }
+            onComplete: function() { send("[NP] Scan fini"); }
         });
-    } catch(e) {}
-
+    } catch(e) { send("[NP] err scan: " + e); }
 });
